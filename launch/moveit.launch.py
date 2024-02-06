5c249c425f760c7eb4e0cdab23291b85ac7ab7a4
mrson88 <sonk9d@gmail.com>
2024-02-06 17:42:20 +0700

    2313

--- launch/moveit.launch.py
+++ launch/moveit.launch.py
@@ -68,6 +68,7 @@ def generate_launch_description():
 
     # MoveIt node
     move_group_node = Node(
+        name="move_group_arm",
         package='moveit_ros_move_group',
         executable='move_group',
         output='screen',
@@ -78,11 +79,14 @@ def generate_launch_description():
                 'robot_description_kinematics': kinematics_config,
                 'ompl': ompl_config,
                 'planning_pipelines': ['ompl'],
+                
             },
+            {'publish_robot_description_semantic': True},
             moveit_controllers,
             trajectory_execution,
             planning_scene_monitor_config,
         ],
+        arguments=["--ros-args", "--log-level", "info"],
     )
 
     # Visualization (parameters needed for MoveIt display plugin)
@@ -104,6 +108,7 @@ def generate_launch_description():
                 'robot_description_kinematics': kinematics_config,
             }
         ],
+        
     )
 
 
@@ -113,3 +118,75 @@ def generate_launch_description():
         ]
 
     )
+
+
+# --------------------------------
+
+# import os
+# from launch import LaunchDescription
+# from moveit_configs_utils import MoveItConfigsBuilder
+# from launch_ros.actions import Node
+# from launch.actions import DeclareLaunchArgument
+# from launch.substitutions import LaunchConfiguration
+# from ament_index_python.packages import get_package_share_directory
+
+
+# def generate_launch_description():
+
+#     is_sim = LaunchConfiguration('is_sim')
+    
+#     is_sim_arg = DeclareLaunchArgument(
+#         'is_sim',
+#         default_value='True'
+#     )
+
+#     moveit_config = (
+#         MoveItConfigsBuilder("robot", package_name="arduinobot_moveit")
+#         .robot_description(file_path=os.path.join(
+#             get_package_share_directory("articubot_one"),
+#             "urdf",
+#             "robot.urdf.xacro"
+#             )
+#         )
+#         .robot_description_semantic(file_path="config/articubot.srdf")
+#         .trajectory_execution(file_path="config/moveit_controllers.yaml")
+#         .to_moveit_configs()
+#     )
+
+#     move_group_node = Node(
+#         package="moveit_ros_move_group",
+#         executable="move_group",
+#         output="screen",
+#         parameters=[moveit_config.to_dict(), 
+#                     {'use_sim_time': is_sim},
+#                     {'publish_robot_description_semantic': True}],
+#         arguments=["--ros-args", "--log-level", "info"],
+#     )
+
+#     # RViz
+#     rviz_config = os.path.join(
+#         get_package_share_directory("articubot_moveit"),
+#             "config",
+#             "moveit1.rviz",
+#     )
+#     rviz_node = Node(
+#         package="rviz2",
+#         executable="rviz2",
+#         name="rviz2",
+#         output="log",
+#         arguments=["-d", rviz_config],
+#         parameters=[
+#             moveit_config.robot_description,
+#             moveit_config.robot_description_semantic,
+#             moveit_config.robot_description_kinematics,
+#             moveit_config.joint_limits,
+#         ],
+#     )
+
+#     return LaunchDescription(
+#         [
+#             is_sim_arg,
+#             move_group_node, 
+#             rviz_node
+#         ]
+#     )
